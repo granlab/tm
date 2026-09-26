bcb56f1430deb945b55d6043327a147b81691a32
granlab <34951612+granlab@users.noreply.github.com>
2026-09-26 16:41:49 +0900

    Update site

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // offline cache: network first (4 s), cache fallback. Caches only same-origin GETs of this static site.
-const C = "tm-mui251no";
+const C = "tm-mui2wmy7";
 self.addEventListener("install", e => { e.waitUntil(caches.open(C).then(c => c.addAll(["./", "./probe/"])).catch(() => {}).then(() => self.skipWaiting())); });
 self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== C).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
 self.addEventListener("fetch", e => {
